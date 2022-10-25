036124fa84af70898b4fe684132dfadd57684c60
AYGJ <jofre1236@gmail.com>
2022-10-24 19:02:15 -0500

    Add load adn updating responsive

--- js/interface.js
+++ js/interface.js
@@ -1,5 +1,5 @@
 const mainCards = document.querySelector(".cards");
-
+const divLoad = document.querySelector(".divLoad");
 export const cards = (response) => {
 
     limpiarHtml(mainCards);
@@ -51,60 +51,79 @@ export const cards = (response) => {
 
 export const getAllInfo = (response) => {
 
-    limpiarHtml(mainCards);
-    response.forEach(country => {
-        const { flags: { png }, name: { common }, currencies, area, timezones, capital, population, region, subregion } = country
-
-        const card = document.createElement("div");
-        card.className = 'card-2';
-
-        const img = document.createElement("img");
-        img.className = 'card2__img';
-        img.src = png;
-
-        const cardInfo = document.createElement("div");
-        cardInfo.className = 'card__info';
-
-        const pCountry = document.createElement("p");
-        pCountry.className = 'card__country'
-        pCountry.textContent = common;
-        const pCapital = document.createElement("p");
-        pCapital.className = 'card__capital';
-        pCapital.textContent = `Capital: ${capital}`;
-        const pPopulation = document.createElement("p");
-        pPopulation.className = 'card__population'
-        pPopulation.textContent = `Población: ${population}`;
-        const pArea = document.createElement("p");
-        pArea.className = 'card__area'
-        pArea.textContent = `Área: ${area}`;
-        const pRegion = document.createElement("p");
-        pRegion.className = 'card__region text-silver';
-        pRegion.textContent = `${region} - ${subregion}`;
-        const pTimeZone = document.createElement("p");
-        pTimeZone.className = 'card__region';
-        pTimeZone.textContent = `Zona horaria: ${timezones[0]}`;
+   
+    divLoad.innerHTML = `   
+    <div class="sk-chase">
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+    </div>
+    `;
+    setTimeout(() => {
+        divLoad.innerHTML= '';
+        limpiarHtml(mainCards);
+        response.forEach(country => {
+            const { flags: { png }, name: { common }, currencies, area, timezones, capital, population, region, subregion } = country
+            const card = document.createElement("div");
+            card.className = 'card-2';
 
 
-        const link = document.createElement("a");
-        link.className = 'card2__link'
-        link.href = "index.html";
-        link.textContent = 'Volver';
+            const img = document.createElement("img");
+            img.className = 'card2__img';
+            img.src = png;
 
-        cardInfo.appendChild(pCountry);
-        cardInfo.appendChild(pCapital);
-        cardInfo.appendChild(pPopulation);
-        cardInfo.appendChild(pArea);
-        cardInfo.appendChild(pRegion);
-        cardInfo.appendChild(pTimeZone);
-        cardInfo.appendChild(link);
+            const cardInfo = document.createElement("div");
+            cardInfo.className = 'card__info';
 
-        card.appendChild(img);
-        card.appendChild(cardInfo);
+            const pCountry = document.createElement("p");
+            pCountry.className = 'card__country'
+            pCountry.textContent = common;
+            const pCapital = document.createElement("p");
+            pCapital.className = 'card__capital';
+            pCapital.textContent = `Capital: ${capital}`;
+            const pPopulation = document.createElement("p");
+            pPopulation.className = 'card__population'
+            pPopulation.textContent = `Población: ${population}`;
+            const pArea = document.createElement("p");
+            pArea.className = 'card__area'
+            pArea.textContent = `Área: ${area}`;
+            const pRegion = document.createElement("p");
+            pRegion.className = 'card__region text-silver';
+            pRegion.textContent = `${region} - ${subregion}`;
+            const pTimeZone = document.createElement("p");
+            pTimeZone.className = 'card__region';
+            pTimeZone.textContent = `Zona horaria: ${timezones[0]}`;
 
-        mainCards.appendChild(card);
 
-    });
+            const link = document.createElement("a");
+            link.className = 'card2__link'
+            link.href = "index.html";
+            link.textContent = 'Volver';
+
+            cardInfo.appendChild(pCountry);
+            cardInfo.appendChild(pCapital);
+            cardInfo.appendChild(pPopulation);
+            cardInfo.appendChild(pArea);
+            cardInfo.appendChild(pRegion);
+            cardInfo.appendChild(pTimeZone);
+            cardInfo.appendChild(link);
+
+            card.appendChild(img);
+            card.appendChild(cardInfo);
+
+            mainCards.appendChild(card);
+
+        });
+    }, 2000);
+
+
+
+
 }
+
 
 function limpiarHtml(father) {
     while (father.firstChild) {
